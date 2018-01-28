cfe26e80c4bb98a05bba73eb8a1918184ab0ab5f
Jerry Huang <jerryhuang@JerrytekiMacBook-Pro.local>
2018-01-28 11:09:51 +0800

    add HWTesting class

---
+++ 美食街/Class/HWTesting.h
@@ -0,0 +1,13 @@
+//
+//  HWTesting.h
+//  美食街
+//
+//  Created by Jerry Huang on 2018/1/28.
+//  Copyright © 2018年 黄炜. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface HWTesting : NSObject
+
+@end
---
+++ 美食街/Class/HWTesting.m
@@ -0,0 +1,13 @@
+//
+//  HWTesting.m
+//  美食街
+//
+//  Created by Jerry Huang on 2018/1/28.
+//  Copyright © 2018年 黄炜. All rights reserved.
+//
+
+#import "HWTesting.h"
+
+@implementation HWTesting
+
+@end
